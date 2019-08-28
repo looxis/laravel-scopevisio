403f6d8a5364089fd2269661fac53ab2b6d92cc8
MannikJ <mannikj@web.de>
2019-08-28 19:22:20 +0200

    Refactor

--- config/scopevisio.php
+++ config/scopevisio.php
@@ -4,9 +4,18 @@
 use Looxis\Laravel\ScopeVisio\ScopeVisio;
 
 return [
-    'customer' => env(ScopeVisio::ENV_SCOPEVISIO_CUSTOMER, ''),
-    'username' => env(ScopeVisio::ENV_SCOPEVISIO_USERNAME, ''),
-    'password' => env(ScopeVisio::ENV_SCOPEVISIO_PASSWORD, ''),
-
-    'pdf_storage_files' => storage_path() . '/app/scopevisio/pdf',
+    'sandbox' => env(ScopeVisio::ENV_SCOPEVISIO_SANDBOX, app()->environment('testing')),
+    'base_uri' => env(ScopeVisio::ENV_SCOPEVISIO_BASE_URI, 'https://appload.scopevisio.com/rest/'),
+    'credentials' => [
+        'production' => [
+            'customer' => env(ScopeVisio::ENV_SCOPEVISIO_CUSTOMER),
+            'username' => env(ScopeVisio::ENV_SCOPEVISIO_USERNAME),
+            'password' => env(ScopeVisio::ENV_SCOPEVISIO_PASSWORD),
+        ],
+        'sandbox' => [
+            'customer' => env(ScopeVisio::ENV_SCOPEVISIO_SANDBOX_CUSTOMER),
+            'username' => env(ScopeVisio::ENV_SCOPEVISIO_SANDBOX_USERNAME),
+            'password' => env(ScopeVisio::ENV_SCOPEVISIO_SANDBOX_PASSWORD),
+        ],
+    ],
 ];
--- src/Services/OutgoingInvoice.php
+++ src/Services/OutgoingInvoice.php
@@ -4,43 +4,46 @@ namespace Looxis\Laravel\ScopeVisio\Services;
 
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
-use Illuminate\Support\Facades\App;
-use Looxis\Laravel\ScopeVisio\ScopeVisio;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class OutgoingInvoice
 {
     /**
-     * @var ScopeVisio
-     */
-    protected $scopeVisio;
-
-    /**
-     * OutgoingInvoice constructor.
-     */
-    public function __construct()
-    {
-
-        $this->scopeVisio = App::make(ScopeVisio::class);
-    }
-
-    /**
      * @param array $formParams
      * @return string
      * @throws GuzzleException
      */
-    public function createInvoice(array $formParams):string
+    public function import(array $formParams): array
     {
-        $url = 'https://appload.scopevisio.com/rest/outgoinginvoices/import';
-        $response = $this->scopeVisio->httpClient->request('POST', $url, [
-            'headers' => [
-                'accept' => 'application/json',
-                'content-type' => 'application/json'
-            ],
-            RequestOptions::JSON => $formParams
-        ]);
-        return $response->getBody()->getContents();
+        $response = \ScopeVisio::client()
+            ->post('outgoinginvoices/import', [
+                RequestOptions::JSON => $formParams
+            ]);
 
+        return json_decode($response->getBody(), true);
+    }
+
+    public function show($number): array
+    {
+        $response = \ScopeVisio::client()->get("outgoinginvoice/$number");
+        return  json_decode($response->getBody(), true);
+    }
+
+    public function search($search = []): array
+    {
+        $response = \ScopeVisio::client()
+            ->post('outgoinginvoices', [
+                RequestOptions::JSON => [
+                    'search' => $search
+                ],
+            ]);
+        return  json_decode($response->getBody(), true);
+    }
+
+
+    public function getFile($number)
+    {
+        return \ScopeVisio::client()->get("outgoinginvoice/$number/file");
     }
 
 
@@ -49,19 +52,16 @@ class OutgoingInvoice
      * @return BinaryFileResponse
      * @throws GuzzleException
      */
-    public function downloadPdf(string $number): BinaryFileResponse
+    public function downloadFile(string $number, $name = null)
     {
-        $url = "https://appload.scopevisio.com/rest/outgoinginvoice/$number/file";
-        $pathDir = config('scopevisio.pdf_storage_files');
-        $filePath = $pathDir . '/' . time() . '_' . $number . '.pdf';
-        if (!file_exists($pathDir)) {
-            mkdir($pathDir, 077, true);
-        }
-        $file_path = fopen($filePath,'w');
-        $this->scopeVisio->httpClient->request('GET', $url, [
-            'sink' => $file_path,
-        ]);
-        return response()->download($filePath);
+        $name = $name ?: $this->buildFileName($number);
+        return response()->streamDownload(function () use ($number) {
+            echo $this->getFile($number)->getBody();
+        }, $name);
     }
 
+    public function buildFileName($number)
+    {
+        return  'invoice_' . $number . '.pdf';
+    }
 }
