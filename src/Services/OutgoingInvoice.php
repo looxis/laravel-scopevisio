0fce1dcc016c8286d4abb6fd5c02002cf0413515
Chris Stefener <cstefener@gmail.com>
2019-11-11 15:33:09 +0100

    add options param to outgoinginvoice

--- src/Services/OutgoingInvoice.php
+++ src/Services/OutgoingInvoice.php
@@ -13,9 +13,9 @@ class OutgoingInvoice
      * @return string
      * @throws GuzzleException
      */
-    public function import(array $formParams): array
+    public function import(array $formParams, array $options): array
     {
-        $response = \ScopeVisio::client()
+        $response = \ScopeVisio::client($options)
             ->post('outgoinginvoices/import', [
                 RequestOptions::JSON => $formParams
             ]);
@@ -23,15 +23,15 @@ class OutgoingInvoice
         return json_decode($response->getBody(), true);
     }
 
-    public function show($number): array
+    public function show($number, array $options): array
     {
-        $response = \ScopeVisio::client()->get("outgoinginvoice/$number");
+        $response = \ScopeVisio::client($options)->get("outgoinginvoice/$number");
         return  json_decode($response->getBody(), true);
     }
 
-    public function search($search = []): array
+    public function search($search = [], array $options): array
     {
-        $response = \ScopeVisio::client()
+        $response = \ScopeVisio::client($options)
             ->post('outgoinginvoices', [
                 RequestOptions::JSON => [
                     'search' => $search
